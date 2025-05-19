67b600d2e8d9371bd372e5b14e5139ba4b5c6049
Constantin Adler <constantin.adler@icloud.com>
2025-05-19 08:40:54 +0200

    Update Fastfile.swift

--- fastlane/swift/Fastfile.swift
+++ fastlane/swift/Fastfile.swift
@@ -2,6 +2,7 @@
 // If you have a custom Fastfile.swift, this file will be replaced by it
 // Don't modify this file unless you are familiar with how fastlane's swift code generation works
 // *** This file will be overwritten or replaced during build time ***
+// Submit a new Beta Build to Apple TestFlight. This will also make sure the profile is up to date
 
 import Foundation
 
@@ -10,6 +11,15 @@ open class Fastfile: LaneFile {
         super.init()
     }
 }
+
+syncCodeSigning(gitUrl: "https://github.com/adlerflow/Fruta-XcodeCloud-Integration.git", appIdentifier: [appIdentifier], username: constantin.adler@icloud.com)
+    // Build your app - more options available
+    buildIosApp(scheme: "SchemeName")
+uploadToTestflight(username: constantin.adler@icloud.com, password: )
+    // You can also use other beta testing services here (run `fastlane actions`)
+
+
+
 
 // Please don't remove the lines below
 // They are used to detect outdated files
